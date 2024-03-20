0a552905e943a99382cb83761c7bc1b27a97c1cb
conquest-bishop <conquestacb@gmail.com>
2024-03-20 13:59:47 +0300

    added upload signature feature

--- src/components/sign.js
+++ src/components/sign.js
@@ -1,20 +1,90 @@
 import React, { useRef, useState } from 'react';
-import { View, Button, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, Platform } from 'react-native';
 import Signature from 'react-native-signature-canvas';
 import { useDispatch } from 'react-redux';
 import { addSignature } from '../redux/reducers/formSlice';
+import * as FileSystem from 'expo-file-system';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import { baseUrl } from '../helpers/config';
 
 import Styles from '../constants/Styles';
 
+const createSignatureFormData = (uri, fileData, name, imageName) => {
+  const Region = fileData.Region;
+  const BranchName = fileData.BranchName;
+  const AgentName = fileData.AgentName;
+  const TempUseSessionId = fileData.TempUseSessionId;
+  const InstCode = fileData.InstitutionCode;
+  const UploadedBy = fileData.Recruiter;
+  const UserId = fileData.UserId;
+  const InstName = fileData.InstName;
+
+  const data = new FormData();
+  data.append(name, {
+    name: imageName,
+    type: 'image/png',
+    uri: Platform.OS === 'android' ? uri : uri.replace('file://', ''),
+  });
+  data.append('Region', Region);
+  data.append('BranchName', BranchName);
+  data.append('AgentName', AgentName);
+  data.append('TempUseSessionId', TempUseSessionId);
+  data.append('InstCode', InstCode);
+  data.append('UserId', UserId);
+  data.append('UploadedBy', UploadedBy);
+  data.append('InstName', InstName);
+  return data;
+};
+
 const SignatureComponent = ({ onSignature, onClear }) => {
   const ref = useRef();
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const [signatureImage, setSignatureImage] = useState(null);
 
+  const uploadBase64 = async (base64String) => {
+    const base64Data = base64String.replace('data:image/png;base64,', '');
+    const filename = 'signature-image-temp.png';
+
+    try {
+      const uri = FileSystem.cacheDirectory + filename;
+      await FileSystem.writeAsStringAsync(uri, base64Data, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+
+      const savedUser = await AsyncStorage.getItem('loginResponse');
+      if (!savedUser) return;
+      const user = JSON.parse(savedUser);
+
+      const formData = createSignatureFormData(
+        uri,
+        user,
+        'Signature',
+        filename
+      );
+
+      // Replace this URL with your server's URL
+      // const serverUrl = `${baseUrl}/AgentFile/uploadApplicationAttachment`;
+
+      // Send the formData to the server
+      await axios.post(
+        `${baseUrl}/AgentFile/uploadApplicationAttachment`,
+        formData
+      );
+    } catch (e) {
+      console.log('*Error*');
+      console.log(e);
+    }
+  };
+
   const handleSignature = (signature) => {
-    dispatch(addSignature(signature));
+    console.log('This is sign', signature);
+    // dispatch(addSignature(signature));
     onSignature(signature);
     setSignatureImage(signature);
+
+    // Call uploadBase64 here
+    uploadBase64(signature);
   };
 
   const handleClear = () => {
@@ -26,11 +96,7 @@ const SignatureComponent = ({ onSignature, onClear }) => {
   return (
     <View>
       <View style={styles.signature}>
-        <Signature 
-          ref={ref}
-          onOK={handleSignature}
-          descriptionText="Sign"
-        />
+        <Signature ref={ref} onOK={handleSignature} descriptionText="Sign" />
       </View>
       {signatureImage && (
         <Image
@@ -38,7 +104,6 @@ const SignatureComponent = ({ onSignature, onClear }) => {
           source={{ uri: signatureImage }}
         />
       )}
-      {/* <Button title="Clear" onPress={handleClear} /> */}
     </View>
   );
 };
---
+++ src/helpers/base64Sign.js
@@ -0,0 +1,61 @@
+const createSignatureFormData = (uri, fileData, name, imageName) => {
+
+    const Region = fileData.Region;
+    const BranchName = fileData.BranchName;
+    const AgentName = fileData.AgentName;
+    const TempUseSessionId = fileData.TempUseSessionId;
+    const InstCode = fileData.InstitutionCode;
+    const UploadedBy = fileData.Recruiter;
+    const UserId = fileData.UserId;
+    const InstName = fileData.InstName
+  
+    const data = new FormData();
+    data.append(name, {
+      name: imageName,
+      type: "image/png",
+      uri: Platform.OS === 'android' ? uri : uri.replace('file://', ''),
+    });
+    data.append('Region', Region);
+    data.append('BranchName', BranchName);
+    data.append('AgentName', AgentName);
+    data.append('TempUseSessionId', TempUseSessionId);
+    data.append('InstCode', InstCode);
+    data.append('UserId', UserId);
+    data.append('UploadedBy', UploadedBy);
+    data.append('InstName', InstName);
+    return data;
+  };
+  
+  const uploadBase64 = async (base64String) => {
+
+    //Without this the FilySystem crashes with 'bad base-64'
+    const base64Data = base64String.replace("data:image/png;base64,","");
+    const filename = 'signature-image-temp.png'
+
+    try {
+      
+      const uri = FileSystem.cacheDirectory + filename;
+      await FileSystem.writeAsStringAsync(
+          uri,
+          base64Data,
+          {
+            'encoding': FileSystem.EncodingType.Base64
+          }
+      )
+
+      const savedUser = await AsyncStorage.getItem('loginResponse');
+      if(!savedUser) return;
+      const user = JSON.parse(savedUser);
+
+      // fileData is current form state
+      const docData = {...fileData, user};
+
+      const formData = createSignatureFormData(uri, docData, 'Signature', filename);
+
+      // Upload this formData
+
+    } catch (e) {
+      console.log('*Error*')
+      console.log(e)
+    }
+  }
